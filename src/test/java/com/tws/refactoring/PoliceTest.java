566bc48266fa87b75c2ff3cc93f0b266de213d8a
JERRY LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIJE9@OOCL.COM>
2019-07-15 14:45:25 +0800

    test:checkDriver is ok

---
+++ src/test/java/com/tws/refactoring/PoliceTest.java
@@ -0,0 +1,42 @@
+package com.tws.refactoring;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PoliceTest {
+
+    @Test
+    public void should_return_true_when_call_checkDriver_given_18_driver(){
+        //given
+        Driver driver = new Driver(18);
+        Police police = new Police();
+        //when
+        Boolean result = police.checkDriver(driver);
+        //then
+        Assertions.assertEquals(true,result);
+
+    }
+
+    @Test
+    public void should_return_false_when_call_checkDriver_given_0_driver(){
+        //given
+        Driver driver = new Driver(0);
+        Police police = new Police();
+        //when
+        Boolean result = police.checkDriver(driver);
+        //then
+        Assertions.assertEquals(false,result);
+    }
+
+//    @Test
+//    public void should_return_false_when_call_checkDriver_given_null_driver(){
+//        //given
+//        Police police = new Police();
+//        //when
+//        Boolean result = police.checkDriver(null);
+//        //then
+//        Assertions.assertEquals(true,result);
+//    }
+}

339548d6eec8198c54c39408d36f63b29ad52a5c
JERRY LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIJE9@OOCL.COM>
2019-07-15 15:42:35 +0800

    refactor:add static final variable LEGAL_AGE of Driver class

--- src/main/java/com/tws/refactoring/Driver.java
+++ src/main/java/com/tws/refactoring/Driver.java
@@ -2,9 +2,10 @@ package com.tws.refactoring;
 
 public class Driver {
     private int age;
+    private static final int LEGAL_AGE = 18;
 
     public void setAge(int age) { this.age = age; }
 
-    public boolean isLegalAge() { return this.age >= 18; }
+    public boolean isLegalAge() { return this.age >= LEGAL_AGE; }
 
 }

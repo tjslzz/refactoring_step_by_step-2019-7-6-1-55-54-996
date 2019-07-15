e53af011522ec7dcbf2aa378e845be788eeee172
JERRY LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIJE9@OOCL.COM>
2019-07-15 14:49:30 +0800

    refactor : make the age of driver private

--- src/main/java/com/tws/refactoring/Driver.java
+++ src/main/java/com/tws/refactoring/Driver.java
@@ -1,9 +1,11 @@
 package com.tws.refactoring;
 
 public class Driver {
-    public int age;
+    private int age;
 
     public Driver(int age) {
         this.age = age;
     }
+
+    public int getAge() { return age; }
 }
